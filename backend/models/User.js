9120a909625a92d19b70248e7b8307eea0ebfe6d
Abhishek0090 <palabhishek411@gmail.com>
2021-09-11 21:12:23 +0530

    Adding Data Valindation

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,4 +21,7 @@ const UserSchema = new Schema({
     }
   });
 
-  module.exports = mongoose.model('user',UserSchema)
+  const User = mongoose.model('user',UserSchema)
+  User.createIndexes(); //creating indexes for different ids in our database
+
+  module.exports = User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 //Creating a User using : POST "/api/auth/" . Doesn't Require Auth
-router.post('/', (req, res) => {
-   console.log(req.body) //for reading the body of api
-   const user = User(req.body)
-   user.save(); //for saving our data in database
-   res.send(req.body) 
-});
+router.post(
+  '/',
+  [
+    body("name", "Enter a Valid Name").isLength({ min: 3 }),
+    body("email", "Enter a Valid email").isEmail,
+    body("password","Password must be of atleast 5 character").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+      }).then(user => res.json(user))
+      .catch(err=>{
+        console.log(err)
+        res.json({error: 'please enter a unique value',message : err.message})
+      })
+  
+  }
+);
 
 module.exports = router;
