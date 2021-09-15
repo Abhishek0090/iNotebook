183f58b61e168bf934b0c539d33fb9807a32bb62
Abhishek0090 <palabhishek411@gmail.com>
2021-09-15 16:31:03 +0530

    uselocation hooks

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,11 @@
 import React,{ useContext, useEffect } from 'react'
-import NoteContext from '../context/notes/NoteContext'
 
 
 export default function About() {
-    const a = useContext(NoteContext)
-    useEffect(() => {
-      a.update();
-       // eslint-disable-next-line
-    }, [])
+
     return (
         <div>
-            This is about {a.state.name} and He is the best {a.state.class}
+            This is about and He is the best
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,21 +3,10 @@ import NoteContext from "./NoteContext";
 import { useState } from "react";
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name" :  "Abhishek",
-        "class" :  "Hacker"
-    }
-    const [state, setstate] = useState(s1)
-    const update = ()=>{
-        setTimeout(() => {
-            setstate({
-                "name" :  "Ackerman",
-                "class" :  "Soldier"
-            })
-        }, 1000);
-    }
+   
+ 
     return(
-        <NoteContext.Provider value={{state,update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )   
